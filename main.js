f526b52e6853a6912803abad616c3aeb09920ab4
Achat-Dev <89579761+Achat-Dev@users.noreply.github.com>
2023-02-16 17:32:34 +0100

    Update main.js

--- main.js
+++ main.js
@@ -5,14 +5,5 @@ const files = [
 files.forEach(generateHTML);
 
 function generateHTML(item) {
-  document.getElementById("download-section").innerHTML = '
-      <hr>
-      
-      <h2>The Scuffed Series - A Clickers Nightmare</h3>
-      <p>Hover over shapes and press the matching buttons to destroy them. Pressing the correct button awards points while pressing the wrong button costs a live. The goal is to break your highscore.</p>
-      <div class="download">
-        <a href="' + item + '.zip' + '" download>
-        <button type="button" class="download-button">Download</button>
-        </a>
-      </div>'
+  document.getElementById("download-section").innerHTML = '\n<hr>\n\n<h2>The Scuffed Series - A Clickers Nightmare</h3><p>Hover over shapes and press the matching buttons to destroy them. Pressing the correct button awards points while pressing the wrong button costs a live. The goal is to break your highscore.</p><div class="download"><a href="' + item + '.zip' + '" download><button type="button" class="download-button">Download</button></a></div>'
 }

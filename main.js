2b8095e356ef9d0aa789a7bc5e0f005ed55a75b3
Achat-Dev <89579761+Achat-Dev@users.noreply.github.com>
2023-02-16 17:49:09 +0100

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-const files = new Map([
-  ["The Scuffed Series - A Clickers Nightmare", "ScuffedSeries_1_ClickersNightmare"],
-]);
-
-files.forEach(generateHTML);
-
-function generateHTML(key, value) {
-  document.getElementById("download-section").innerHTML = '\n<hr>\n\n<h2>' + key + '</h2><p>Hover over shapes and press the matching buttons to destroy them. Pressing the correct button awards points while pressing the wrong button costs a live. The goal is to break your highscore.</p><div class="download"><a href="' + value + '.zip' + '" download><button type="button" class="download-button">Download</button></a></div>'
-}

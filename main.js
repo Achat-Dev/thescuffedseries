7dc8149a9810e0448df28e0a34644d8d73aa0433
Achat-Dev <89579761+Achat-Dev@users.noreply.github.com>
2023-02-16 17:27:21 +0100

    Create main.js

---
+++ main.js
@@ -0,0 +1,18 @@
+const files = [
+  "ScuffedSeries_1_ClickersNightmare"
+];
+
+files.forEach(generateHTML);
+
+function generateHTML(item) {
+  document.getElementById("download-content").innerHTML = '
+      <hr>
+      
+      <h2>The Scuffed Series - A Clickers Nightmare</h3>
+      <p>Hover over shapes and press the matching buttons to destroy them. Pressing the correct button awards points while pressing the wrong button costs a live. The goal is to break your highscore.</p>
+      <div class="download">
+        <a href="' + item + '.zip' + '" download>
+        <button type="button" class="download-button">Download</button>
+        </a>
+      </div>'
+}
